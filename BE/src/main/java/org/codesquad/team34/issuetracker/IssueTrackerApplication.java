349f31199c2df748b7569728987bc7fc946667d5
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-06-14 17:40:40 +0900

    Chore: Spring Boot initialization

---
+++ BE/src/main/java/org/codesquad/team34/issuetracker/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.codesquad.team34.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/org/codesquad/team34/issuetracker/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codesquad.team34.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
