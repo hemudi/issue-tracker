57c55d6f6be6cf6049968a1a6311e5abbb433fed
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-06-16 21:33:39 +0900

    Feat: Member 엔티티 작성 및 DB 연동 설정
    jidonny/issue-tracker#8

---
+++ BE/src/main/java/org/codesquad/team34/issuetracker/common/BaseEntity.java
@@ -0,0 +1,17 @@
+package org.codesquad.team34.issuetracker.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
