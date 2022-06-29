7ae0cfed0afb3cdb28a7114e982cfe0ceb959d00
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-06-29 21:02:05 +0900

    Feat: 현재 사용자 정보 조회
    jidonny/issue-tracker#25

--- BE/src/main/java/org/codesquad/team34/issuetracker/member/MemberController.java
+++ BE/src/main/java/org/codesquad/team34/issuetracker/member/MemberController.java
@@ -1,9 +1,12 @@
 package org.codesquad.team34.issuetracker.member;
 
+import org.codesquad.team34.issuetracker.common.LoginInterceptor;
+import org.codesquad.team34.issuetracker.member.dto.MemberDto;
 import org.codesquad.team34.issuetracker.member.dto.MemberListResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,12 @@ public class MemberController {
     @GetMapping
     public ResponseEntity<MemberListResponse> listMembers(Pageable pageable) {
         return ResponseEntity.ok(memberService.findAll(pageable));
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<MemberDto> currentUser(@
+        RequestAttribute(name = LoginInterceptor.CURRENT_LOGIN) MemberDto currentMember) {
+
+        return ResponseEntity.ok(currentMember);
     }
 }
